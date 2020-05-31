846313a693740a5b1e95f26d2d0cfe29da6d488c
amynordrum <65627338+amynordrum@users.noreply.github.com>
2020-05-31 14:56:46 -0400

    Added product count

--- groceries.py
+++ groceries.py
@@ -66,8 +66,13 @@ print("--------------")
 print("THERE ARE " + str(department_count) + " DEPARTMENTS:")
 print("--------------")
 
+departments.sort()
+
 for d in departments: 
-    print(d)
+    matching_products = [p for p in products if p["department"] == d]
+    matching_products_count = len(matching_products)
+    print(d.title() + " (" + str(matching_products_count) + " products)")
+
 
 
 #  "id":17
